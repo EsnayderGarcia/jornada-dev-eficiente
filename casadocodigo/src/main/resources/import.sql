a0c61e8a5974e0d9ce7951d1744d6f5c937a0d84
EsnayderGarcia <scriptk.18@gmail.com>
2023-09-05 19:09:28 -0300

    seed para as tabelas de países e estados

--- casadocodigo/src/main/resources/import.sql
+++ casadocodigo/src/main/resources/import.sql
@@ -8,6 +8,15 @@ INSERT INTO tb_autor (nome, descricao, email, instante_criacao) VALUES ('Stephen
 INSERT INTO tb_autor (nome, descricao, email, instante_criacao) VALUES ('Robert Kyosaki', 'Um autor muito top!', 'robert@gmail.com', '2023-07-13');
 INSERT INTO tb_autor (nome, descricao, email, instante_criacao) VALUES ('George R.R Martir', 'Um autor muito top!', 'george@gmail.com', '2023-07-13');
 
+INSERT INTO tb_pais(nome) VALUES ('Brasil');
+INSERT INTO tb_pais(nome) VALUES ('Argentina');
+INSERT INTO tb_pais(nome) VALUES ('Equador');
+INSERT INTO tb_pais(nome) VALUES ('Colômbia');
+
+INSERT INTO tb_estado(nome, pais_id) VALUES ('Pará', 1);
+INSERT INTO tb_estado(nome, pais_id) VALUES ('Maranhão', 1);
+INSERT INTO tb_estado(nome, pais_id) VALUES ('Chimborazo', 3);
+
 INSERT INTO tb_livro (titulo, resumo, sumario, preco, paginas, isbn, data_publicacao, categoria_id, autor_id) VALUES ('Game of Thrones', 'Livro muito bom.', 'O foco central de GoT acomapanha o jogo político em Westeros, o continente de um mundo fantástico onde há regiões governadas por Casas nobres. Há sete reinos que estão sob a liderança do rei ocupante do Trono de Ferro, uma posição alta e desejada entre boa parte das famílias.', 58.40, 110, 'PA312123', '2024-10-11', 4, 4);
 INSERT INTO tb_livro (titulo, resumo, sumario, preco, paginas, isbn, data_publicacao, categoria_id, autor_id) VALUES ('Pai Rico, Pai Pobre', 'Livro muito bom.', 'Pai Rico, Pai Pobre é o primeiro best-seller de Robert Kiyosaki e Sharon Lechter. Ele advoga a busca pela independência financeira através de investimento, imóveis, ter seu próprio negócio e o uso de táticas financeiras de proteção do patrimônio.', 61.50, 85, 'PA311123', '2024-10-11', 2, 3);
 INSERT INTO tb_livro (titulo, resumo, sumario, preco, paginas, isbn, data_publicacao, categoria_id, autor_id) VALUES ('IT - A Coisa', 'Livro muito bom.', 'It é um romance de terror de 1986 do autor estadunidense Stephen King. Foi seu 22º livro e seu 17º romance escrito em seu próprio nome. A história segue as experiências de sete crianças, que são aterrorizadas por uma entidade maligna que explora os medos de suas vítimas para se disfarçar enquanto caçam suas presas.', 67.50, 110, 'PA312103', '2024-10-11', 3, 2);
